b8598c55c32c300ac4ed95d42371ac572dce1ebf
sere06001 <sere06001@utb.vaxjo.se>
2024-02-02 12:42:54 +0100

    Add project files.

---
+++ Uppgift3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            map["Sverige"] = "450 000km^2";
+            map["Norge"] = "385 000km^2";
+            map["Danmark"] = "43 000km^2";
+            map["Finland"] = "338 000km^2";
+
+            Console.WriteLine("Skriv \"Sverige\", \"Norge\", \"Danmark\" eller \"Finland\" så kommer programmet att skriva ut landets area.");
+            string svar = Console.ReadLine();
+            Console.WriteLine($"{svar} har en area på {map[svar]}");
+            Console.ReadKey();
+        }
+    }
+}
